7a391419de4f3d8db78622dfaad3fbb1d1c596de
santoh15 <santihe15@hotmail.com>
2025-12-13 22:57:15 +0000

    Creando entorno gunicorn

--- predict.py
+++ predict.py
@@ -1,8 +1,27 @@
 import pickle
+from flask import Flask
+from flask import request
+from flask import jsonify
 
 model_file='model_c=1.0.bin'
 with open(model_file, 'rb') as f_in:
     dv,model=pickle.load(f_in)
 
-#diccionario del cliente
 
+app= Flask('churn')
+
+@app.route('/predict', methods=['POST'])
+
+
+def predict():
+    customer = request.get_json()
+    X=dv.transform([customer])
+    y_pred=model.predict_proba(X)[0,1]
+    churn = y_pred >= 0.5
+    results= {'churn_probability': float(y_pred),
+              'churn': bool(churn)}
+
+    return jsonify(results)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=6565)
